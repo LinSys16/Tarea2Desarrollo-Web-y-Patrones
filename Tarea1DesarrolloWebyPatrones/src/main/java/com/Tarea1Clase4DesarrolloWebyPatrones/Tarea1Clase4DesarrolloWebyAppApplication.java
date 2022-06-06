ab7abc0bf9a6c8345bc36716f787e53f461521c4
LinSys16 <badillalindsay85@gmail.com>
2022-06-06 16:23:34 -0600

    Tarea 2. Desarrollo Web y Patrones. Mi primer Tarea. Página de Dibujo.

---
+++ Tarea1DesarrolloWebyPatrones/src/main/java/com/Tarea1Clase4DesarrolloWebyPatrones/Tarea1Clase4DesarrolloWebyAppApplication.java
@@ -0,0 +1,13 @@
+package com.Tarea1Clase4DesarrolloWebyPatrones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea1Clase4DesarrolloWebyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea1Clase4DesarrolloWebyAppApplication.class, args);
+	}
+
+}
---
+++ Tarea1DesarrolloWebyPatrones/src/test/java/com/Tarea1Clase4DesarrolloWebyPatrones/Tarea1Clase4DesarrolloWebyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tarea1Clase4DesarrolloWebyPatrones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea1Clase4DesarrolloWebyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
